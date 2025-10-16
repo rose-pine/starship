35859238bc699785430de84a27704b8560c1c2c4
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2025-10-16 13:31:27 +1000

    fix newline stuff

--- .github/scripts/build-configs.sh
+++ .github/scripts/build-configs.sh
@@ -27,7 +27,7 @@ while read -r cfg; do
   if [ -z "$name" ] || [ "$name" = "null" ]; then
     echo "⚠️ Skipping invalid config: $cfg" >&2
     continue
-  fi
+  # fi
 
   output="$OUT_DIR/$name"
   rm -rf "$output"
@@ -69,11 +69,12 @@ while read -r cfg; do
     outputp="$output/$pal.toml"
     : > "$outputp"
 
+    format_line=$(printf "%s\\\n" "${format_parts[@]}")
     cat > "$outputp" <<EOF
 "\$schema" = 'https://starship.rs/config-schema.json'
 
 format = """
-$(printf "%s\\\n" "${format_parts[@]}")
+$(format_line)
 """
 EOF
 
